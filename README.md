a2dd31a89b13902087b39355a9b0e9fce1dc0692
URIAN VIERA <urian1213viera@gmail.com>
2024-04-01 10:45:47 -0500

    iniciando dev

---
+++ README.md
@@ -0,0 +1,33 @@
+# Cómo Enviar Formularios desde React hacia PHP, Paso a Paso y sin Estrés
+
+##### Este proyecto se centra en la creación de un formulario en ReactJS que permita a los usuarios enviar datos a un servidor PHP. A través de esta integración, se busca facilitar la comunicación entre el front-end y el back-end, permitiendo el procesamiento y almacenamiento de la información proporcionada por los usuarios de manera eficiente y segura.
+
+##### Instalar axios, para mejorar las solicitudes HTTP a la API
+
+    npm i axios
+
+#### Instalar la libreria react-toastify para crear las alertas.
+
+    npm install --save react-toastify
+
+#### Para correr el proyecto, solo basta instalar las dependencias y correr el proyecto
+
+    npm install
+    npm run dev
+
+#### Resultado final
+
+![](https://raw.githubusercontent.com/urian121/imagenes-proyectos-github/master/agenda-contactos-react-php-y-mysql.png)
+
+##### Notas
+
+    El formulario se envia al backend que esta en php, la ubicacion seria 'backend-php/index.php' esta carpeta si debe estar en un servidor de apache tal vez xampp,. Desde React enviaremos todos los datos del formulario al 'backend-php/index.php' luego php con php capturaremos los datos que se reciben.
+
+### Expresiones de Gratitud 🎁
+
+    Comenta a otros sobre este proyecto 📢
+    Invita una cerveza 🍺 o un café ☕
+    Paypal iamdeveloper86@gmail.com
+    Da las gracias públicamente 🤓.
+
+## No olvides SUSCRIBIRTE 👍
---
+++ backend-php/index.php
@@ -0,0 +1,61 @@
+<?php
+
+// Establecer encabezados CORS para permitir solicitudes desde cualquier origen
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+require('configBD.php');
+$metodo = $_SERVER['REQUEST_METHOD'];
+$tbl_amigos = 'tbl_amigos';
+$dirLocal = "fotos_amigos";
+$extensionesPermitidas = array("jpg", "jpeg", "png", "gif");
+
+switch ($metodo) {
+    case 'GET':
+        $query = "SELECT * FROM $tbl_amigos ORDER BY id DESC";
+        $resultado = mysqli_query($con, $query);
+
+        $usuarios = array();
+        while ($fila = mysqli_fetch_assoc($resultado)) {
+            $usuarios[] = $fila;
+        }
+        echo json_encode($usuarios);
+        break;
+
+    case 'POST':
+        if (isset($_FILES['avatar'])) {
+            $archivoTemporal = $_FILES['avatar']['tmp_name'];
+            $nombreArchivo = $_FILES['avatar']['name'];
+
+            $extension = strtolower(pathinfo($nombreArchivo, PATHINFO_EXTENSION));
+
+            if (in_array($extension, $extensionesPermitidas)) {
+                // Generar un nombre único y seguro para el archivo
+                $nombreArchivo = substr(md5(uniqid(rand())), 0, 10) . "." . $extension;
+                $rutaDestino = $dirLocal . '/' . $nombreArchivo;
+
+                // Mover el archivo a la ubicación deseada
+                if (move_uploaded_file($archivoTemporal, $rutaDestino)) {
+                    $nombre = ucwords($_POST['nombre']);
+                    $email = trim($_POST['email']);
+                    $telefono = trim($_POST['telefono']);
+
+                    $query = "INSERT INTO $tbl_amigos (nombre, email, telefono, avatar) VALUES ('$nombre', '$email', '$telefono', '$nombreArchivo')";
+                    if (mysqli_query($con, $query)) {
+                        echo json_encode(array('message' => 'Nuevo amigo creado correctamente'));
+                    } else {
+                        echo json_encode(array('error' => 'Error al crear amigo: ' . mysqli_error($con)));
+                    }
+                } else {
+                    echo json_encode(array('error' => 'Error al mover el archivo'));
+                }
+            } else {
+                echo json_encode(array('error' => 'Tipo de archivo no permitido'));
+            }
+        }
+        break;
+}
+mysqli_close($con);
